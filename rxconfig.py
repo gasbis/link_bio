32f94c3623ee09fab91f364af2c3898ff15a9ddb
gasbis <gasbis@hotmail.com>
2026-07-30 20:26:24 +0200

    conexion backend

--- rxconfig.py
+++ rxconfig.py
@@ -2,6 +2,8 @@ import reflex as rx
 
 config = rx.Config(
     app_name="link_bio_1963",
+    #la api solo es en caso de desplegar el backend en servidor distinto
+    api_url="https://linkbio-production-7687.up.railway.app:8000",
     plugins=[
         rx.plugins.SitemapPlugin(),
         rx.plugins.TailwindV4Plugin(),
