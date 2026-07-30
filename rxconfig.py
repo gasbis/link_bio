0c87cf9e2c38b7b479b010640792fc1fa473fa7e
gasbis <gasbis@hotmail.com>
2026-07-30 20:37:15 +0200

    corregido error api

--- rxconfig.py
+++ rxconfig.py
@@ -3,7 +3,7 @@ import reflex as rx
 config = rx.Config(
     app_name="link_bio_1963",
     #la api solo es en caso de desplegar el backend en servidor distinto
-    api_url="https://linkbio-production-7687.up.railway.app:8000",
+    api_url="https://linkbio-production-7687.up.railway.app/",
     plugins=[
         rx.plugins.SitemapPlugin(),
         rx.plugins.TailwindV4Plugin(),
