22d78d26a7bc780443a7ae48b9e1646194fd2501
gasbis <gasbis@hotmail.com>
2026-07-31 11:46:05 +0200

    otra api

--- rxconfig.py
+++ rxconfig.py
@@ -3,7 +3,7 @@ import reflex as rx
 config = rx.Config(
     app_name="link_bio_1963",
     #la api solo es en caso de desplegar el backend en servidor distinto
-    #api_url="https://linkbio-gasbis-back.up.railway.app/",
+    api_url="https://linkbio-gasbis-back.up.railway.app",
     plugins=[
         rx.plugins.SitemapPlugin(),
         rx.plugins.TailwindV4Plugin(),
